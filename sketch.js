3812049a063f573585c8b02f352fe6eae1351d03
minialappatt <72683924+minialappatt@users.noreply.github.com>
2021-08-07 03:48:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
+  
+// create left Boundary
+
+
+//create right Boundary
+
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  // boy moving on Xaxis with mouse
+  
+  
+  //code to reset the background
+  
+  
+  drawSprites();
+}
